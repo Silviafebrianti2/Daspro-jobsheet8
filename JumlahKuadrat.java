cb87c3c97895b0957c89f50d697959bd056ea42b
Silviafebrianti2 <silviafebrianti517@gmail.com>
2025-10-29 10:17:54 +0700

    tugas2

--- JumlahKuadrat.java
+++ JumlahKuadrat.java
@@ -6,17 +6,14 @@ public class JumlahKuadrat {
         System.out.print("Masukkan nilai n: ");
         int n = sc.nextInt();
 
-        // Outer loop untuk setiap nilai dari 1 hingga n
         for (int i = 1; i <= n; i++) {
             int jumlahKuadrat = 0;
             System.out.print("n = " + i + " → jumlah kuadrat = ");
 
-            // Inner loop untuk menghitung kuadrat dari 1 sampai i
             for (int j = 1; j <= i; j++) {
                 int kuadrat = j * j;
                 jumlahKuadrat += kuadrat;
 
-                // Menampilkan setiap kuadrat dengan format
                 System.out.print(kuadrat);
                 if (j < i) {
                     System.out.print(" + ");
