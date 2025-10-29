dea61dfa7ad0af7b92b6fe2a3bc77d73bfbcce14
Silviafebrianti2 <silviafebrianti517@gmail.com>
2025-10-29 10:00:07 +0700

    percobaan4

---
+++ NilaiKelompok26.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class NilaiKelompok26 { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;        
+        int j;           
+        int nilai;       
+        float totalNilai; 
+        float rataNilai;  
+
+        System.out.println("=== Program Perhitungan Nilai Kelompok ===");
+
+       
+        while (i <= 6) {
+            System.out.println("\nKelompok " + i + ":");
+
+            totalNilai = 0; 
+
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Masukkan nilai dari Kelompok Penilai ke-" + j + ": ");
+                nilai = sc.nextInt();
+
+                while (nilai < 1 || nilai > 100) {
+                    System.out.print("Nilai tidak valid! Masukkan nilai antara 1-100: ");
+                    nilai = sc.nextInt();
+                }
+
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+            System.out.println("Total Nilai Kelompok " + i + " = " + totalNilai);
+            System.out.println("Rata-rata Nilai Kelompok " + i + " = " + rataNilai);
+            System.out.println("-------------------------------------------");
+
+            i++;
+        }
+
+        System.out.println("=== Program Selesai ===");
+        sc.close();
+    }
+}
